BUG: overflow errors - to check by colleague

Methods/NewtonMethod.py
@@ -72,14 +72,10 @@ class NewtonMethod:
                     except LinAlgError:
                         tau = max(2 * tau, B)
 
-                if 'R' not in locals():
-                    print(f"Failed to factorize Hessian at iter {k} after 20 attempts.")
-                    return x, np.array(path), np.linalg.norm(gradient), False, k
                
                 p_mn = cho_solve_banded((R,True), -gradient)
                 #check
-                grad_norm = np.linalg.norm(gradient)
-                print(f"Iter {k}: Grad Norm = {grad_norm:.4e}, Alpha = {alpha}")
+                
 
                 alpha = bcktrk.backtrack(p_mn,x,problem.function,1,gradient)
                 

Problems/Problem_64.py
@@ -7,6 +7,11 @@ class Problem_64:
         self.n = n
         self.rho = rho
         self.h = 1.0/(n+1)
+
+    def _safe_arg(self, x):
+        # Clamps the input to the range [-700, 700]
+        # exp(710) is the limit for 64-bit floats.
+        return np.clip(self.rho * x, -300, 300)
     
 
 # Returns vector f_k(x) 
@@ -17,8 +22,10 @@ class Problem_64:
         n = self.n
         f = np.zeros(n)
         
+        arg = self._safe_arg(x)
+
         # intermediate term: common to all indexes
-        term = 2 * x +(rho * h**2) * np.sinh(rho * x)
+        term = 2 * x +(rho * h**2) * np.sinh(arg)
 
         # construction of all terms
         # k = 1 (=0 for python indexes)
@@ -49,8 +56,10 @@ class Problem_64:
         h = self.h
         grad = np.zeros(n)
 
+        arg = self._safe_arg(x)
+
         # computation of d(f_k) / d(x_k)
-        diag = 2 + ((rho**2) * (h**2) * np.cosh(rho * x))
+        diag = 2 + ((rho**2) * (h**2) * np.cosh(arg))
 
         # contribution of [f_k] (for all k)
         grad += f * diag
@@ -73,8 +82,10 @@ class Problem_64:
         rho = self.rho
         h = self.h
 
+        arg = self._safe_arg(x)
+
         # computation of d(f_k) / d(x_k)
-        diag = 2 + ((rho**2) * (h**2) * np.cosh(rho * x))
+        diag = 2 + ((rho**2) * (h**2) * np.cosh(arg))
         
         # computation of d(f_k) / d(x_{k-1}
         lower_diag = -1 * np.ones(n-1)
@@ -89,7 +100,7 @@ class Problem_64:
         first_order = J.T @ J
         
         # computation of d^2(f_k) / d(x_k)^2
-        diag_2 =  ((rho**3) * (h**2) * np.sinh(rho * x)) * f
+        diag_2 =  ((rho**3) * (h**2) * np.sinh(arg)) * f
         
         # second order terms
         second_order = diags(diag_2, 0, shape=(n,n), format='csr')

provisory_main.py
@@ -5,7 +5,7 @@ from Methods.NewtonMethod import NewtonMethod
 from Problems.Problem_64 import Problem_64
 import matplotlib.pyplot as plt
 from matplotlib.widgets import Slider
-from Methods.TruncatedNewton_provisory import truncated_newton
+from Methods.TruncatedNewtonMethod import TruncatedNewtonMethod
 from scipy.optimize import minimize
 
 def main():
@@ -34,6 +34,7 @@ def main():
         print(f"\n=== Running experiments with tol = {tol:.1e} ===")
 
         modified_newt = NewtonMethod(tol, 1000, 0.5, 0.5)
+        truncated_newt = TruncatedNewtonMethod(tol, 1000, 1000, 'sl')
 
         x_initial_results = {}
         x_initial_results_tr = {}
@@ -41,7 +42,7 @@ def main():
         x_random_results = {}
         x_random_results_tr = {}
 
-        problem_64 = Problem_64(50, 10)
+        '''problem_64 = Problem_64(50, 10)
         my_x, _, _, _, _ = modified_newt.minimize(problem_64, x0_50, mode="exact")
 
         # 2. Solve with SCIPY
@@ -54,7 +55,7 @@ def main():
 
         # 3. Compare
         diff = np.linalg.norm(my_x - scipy_res.x)
-        print(f"Difference between My Newton and Scipy w tol {tol}: {diff:.2e}")
+        print(f"Difference between My Newton and Scipy w tol {tol}: {diff:.2e}")'''
 
 
         
@@ -73,7 +74,7 @@ def main():
 
             #TR
             start_time_tr = time.time()
-            x_tr, norm_gradient_tr, converges_tr, steps_tr, path_tr = truncated_newton(problem_64.function, problem_64.gradient, problem_64.hessian, starting_point, tol, 1000, 1000, 'sl')
+            x_tr, norm_gradient_tr, converges_tr, steps_tr, path_tr = truncated_newt.truncated_newton(problem_64.function, problem_64.gradient, problem_64.hessian, starting_point)
             end_time_tr = time.time() - start_time_tr
 
             final_score_tr = problem_64.function(x_tr)
@@ -158,10 +159,10 @@ def main():
                 iterations.append(steps)
                 converges_list.append(converges)
 
-                '''if tol == 1e-4:
+                if tol == 1e-4:
 
                     start_time_tr = time.time()
-                    x_tr, norm_gradient_tr, converges_tr, steps_tr, path_tr = truncated_newton(problem_64.function, problem_64.gradient, problem_64.hessian, starting_point,tol,1000,1000,'sl')
+                    x_tr, norm_gradient_tr, converges_tr, steps_tr, path_tr = truncated_newt.truncated_newton(problem_64.function, problem_64.gradient, problem_64.hessian, starting_point,tol,1000,1000,'sl')
                     end_time_tr = time.time() - start_time_tr
 
                     final_score_tr = problem_64.function(x_tr)
@@ -171,7 +172,7 @@ def main():
                     final_scores_tr.append(final_score_tr)
                     iterations_tr.append(steps_tr)
                     converges_list_tr.append(converges_tr)
-                    path_history_tr.append(path_tr)'''
+                    path_history_tr.append(path_tr)
 
 
             x_random_results[n_dim] = {